c7dc03b1562d7578f47c414a800e78196d891708
vceqiv09 <vceqiv@gmail.com>
2022-11-17 17:50:53 +0900

    AbstractClass 1117 first

---
+++ arrange_object/src/abstractClass/Customer.java
@@ -0,0 +1,14 @@
+package abstractClass;
+
+public class Customer extends Person{
+	
+	@Override
+	public String getFirstName() {
+		return getName().substring(0, 1);
+	}
+	@Override
+	public String getLastName() {
+		return getName().substring(1);
+	}
+	
+}
---
+++ arrange_object/src/abstractClass/Person.java
@@ -0,0 +1,51 @@
+package abstractClass;
+
+import java.util.Objects;
+
+public abstract class Person {
+	
+	private String name;
+	private int age;
+	private char gender;
+
+	// 추상 메서드
+	public abstract String getFirstName();
+	public abstract String getLastName();
+	
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public char getGender() {
+		return gender;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, gender, name);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return age == other.age && gender == other.gender && Objects.equals(name, other.name);
+	}
+	
+}
