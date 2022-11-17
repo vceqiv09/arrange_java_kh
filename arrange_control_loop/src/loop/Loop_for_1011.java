5361329b90c7933db93e5a2dcc15d55615c65d7a
vceqiv09 <vceqiv@gmail.com>
2022-11-17 17:24:45 +0900

    Loop_for 1117 first

---
+++ arrange_control_loop/src/loop/Loop_for_1011.java
@@ -0,0 +1,18 @@
+package loop;
+
+public class Loop_for_1011 {
+
+	public static void main(String[] args) {
+		// for문
+		
+		//중첩 for문
+		for(int i=1; i<10; i++) {
+			for(int j=1; j<10; j++) {
+				System.out.print(j+ " * " +i+ "\t");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
