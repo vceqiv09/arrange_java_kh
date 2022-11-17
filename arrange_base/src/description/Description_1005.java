517fc0f83cc3a7e48be3c75aed8de17f47071734
vceqiv <vceqiv@vceqiv>
2022-11-17 15:55:40 +0900

    Description 1117 first

---
+++ arrange_base/src/description/Description_1005.java
@@ -0,0 +1,19 @@
+package description;
+/**
+ * 블럭 주석이긴 하지만
+ * 클래스나 메서드에 대한 설명을 작성하기 위한 용도로 사용
+ * @author user2
+ *
+ */
+public class Description_1005 {
+	/**
+	 * 
+	 * @param args		args라는 파라미터가 뭔지 설명하는 주석
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		System.out.println("Hi!!!");
+
+	}
+
+}
